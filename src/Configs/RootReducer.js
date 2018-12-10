c6927804b4d92d00117c43a433c52009a6b7b4bd
Alexander Tartmin <alexander@tartmin.name>
2018-12-10 20:04:14 +0200

    Registered User Module in the Store

--- src/Configs/RootReducer.js
+++ src/Configs/RootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import appState from '../Modules/App/reducer';
+import userState from '../Modules/User/reducer';
 
 export default combineReducers({
   appState,
+  userState,
 });
--- src/Configs/RootSaga.js
+++ src/Configs/RootSaga.js
@@ -1,9 +1,11 @@
 import { all } from 'redux-saga/effects';
 
 import appSaga from '../Modules/App/saga';
+import userSaga from '../Modules/User/saga';
 
 export default function* root() {
   yield all([
     appSaga(),
+    userSaga(),
   ]);
 }
