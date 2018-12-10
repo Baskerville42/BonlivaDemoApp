0c0f8022b3815681214a3a51d6b8de1f7606a9b8
Alexander Tartmin <alexander@tartmin.name>
2018-12-10 20:06:05 +0200

    Merge some files into one Configured root component of the application

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry, YellowBox } from 'react-native';
+import { Provider } from 'react-redux';
+
 import {name as appName} from './app.json';
+import Store from './src/Configs/Store';
+import RootStack from './src/Configs/Navigator';
+import NavigationService from './src/Services/Navigation';
+
+YellowBox.ignoreWarnings(['unknown call: "relay:check"']);
+
+const App = () => (
+  <Provider store={Store}>
+    <RootStack ref={navigatorRef => NavigationService.setTopLevelNavigator(navigatorRef)} />
+  </Provider>
+);
 
 AppRegistry.registerComponent(appName, () => App);
